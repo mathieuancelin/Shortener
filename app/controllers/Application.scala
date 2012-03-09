71cc4917d28b69af02c1308a494ea6a52c47654a
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2012-03-09 20:12:19 +0100

    Fix style

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,60 +11,60 @@ import models._
  */
 object Application extends Controller {
 	/** Get the minified prefix from configuration **/
-	val base = Play.current.configuration.getString( "shorten.base" ).getOrElse( "http://fakebase/" )
+    val base = Play.current.configuration.getString( "shorten.base" ).getOrElse( "http://fakebase/" )
 
-	val redirect = base
-	/** Pattern to validate URL, very dumb **/
-	val pattern = "^(https?)://.+$".r
+    val redirect = base
+    /** Pattern to validate URL, very dumb **/
+    val pattern = "^(https?)://.+$".r
 
-	val start = "http://"
-	/** Dataform representing posted values for Shorten creation **/
-	val urlForm = Form( "url" -> text )
+    val start = "http://"
+    /** Dataform representing posted values for Shorten creation **/
+    val urlForm = Form( "url" -> text )
   
-  	/** Index rendering **/
+    /** Index rendering **/
     def index = Action {
         Ok( views.html.index() )
     }
 
     /** Add a new Shorten on the database **/
     def addShorten = Action { implicit request =>
-    	//val maybeUrlValue = urlForm.bindFromRequest.get
-    	urlForm.bindFromRequest.fold (
-    		formWithErrors => BadRequest( "You need to post a 'url' value!" ),
-	    	{ maybeUrlValue =>
-	    		val url = Option.apply( maybeUrlValue ) filter { !_.startsWith(start) } toRight ( maybeUrlValue ) fold ( identity, { start + _ } )
-	    		url match {
-	    			case pattern(v) => Ok( redirect + Shorten.getShortenFor ( url ).id )
-	    			case _ => Ok( "wrong" )
-	    		}
-	    	}
-    	)
-	}
+        //val maybeUrlValue = urlForm.bindFromRequest.get
+        urlForm.bindFromRequest.fold (
+            formWithErrors => BadRequest( "You need to post a 'url' value!" ),
+            { maybeUrlValue =>
+                val url = Option.apply( maybeUrlValue ) filter { !_.startsWith(start) } toRight ( maybeUrlValue ) fold ( identity, { start + _ } )
+                url match {
+                    case pattern(v) => Ok( redirect + Shorten.getShortenFor ( url ).id )
+                    case _ => Ok( "wrong" )
+                }
+            }
+        )
+    }
 	
-	/** Render all existing Shortens **/
-	def all = Action {
-		Ok( views.html.all( Shorten.findAll(), redirect ) )
-	}
+    /** Render all existing Shortens **/
+    def all = Action {
+        Ok( views.html.all( Shorten.findAll(), redirect ) )
+    }
 	
-	/** Redirect a request from minified URL to actual URL **/
-	def redirectTo( id: Long ) = Action {
-		Redirect( Shorten.findById ( id ) map { _.url } getOrElse ( base ) );
-	}
+    /** Redirect a request from minified URL to actual URL **/
+    def redirectTo( id: Long ) = Action {
+        Redirect( Shorten.findById ( id ) map { _.url } getOrElse ( base ) );
+    }
 
-	/** Render about view **/
-	def about = Action {
-		Ok( views.html.about() )
-	}
+    /** Render about view **/
+    def about = Action {
+        Ok( views.html.about() )
+    }
  
- 	/** Delete a particular Shorten **/
-	def delete( id: Long ) = Action {
-		Shorten.delete( id )
-		Redirect( routes.Application.all() )
-	}
+    /** Delete a particular Shorten **/
+    def delete( id: Long ) = Action {
+        Shorten.delete( id )
+        Redirect( routes.Application.all() )
+    }
 
- 	/** Delete all Shortens **/
-	def deleteAll() = Action {
-		Shorten.deleteAll()
-		Redirect( routes.Application.all() )
-	}
+    /** Delete all Shortens **/
+    def deleteAll() = Action {
+        Shorten.deleteAll()
+        Redirect( routes.Application.all() )
+    }
 }
--- app/models/Models.scala
+++ app/models/Models.scala
@@ -15,11 +15,11 @@ case class Shorten( id: Long = -1L, url: String )
  **/
 class EnhancedShorten( shorten: Shorten ) {
 
-	def save() = Shorten.save( shorten )
+    def save() = Shorten.save( shorten )
 
-	def delete() = Shorten.delete( shorten.id )
+    def delete() = Shorten.delete( shorten.id )
 
-	def exists() = Shorten.exists( shorten ) 
+    def exists() = Shorten.exists( shorten ) 
 }
 
 /** 
@@ -31,120 +31,120 @@ object Shorten {
 	 * implicit conversion from Shorten to EnhancedShorten to 
 	 * automatically add utilities methods to Shorten case class
 	 **/
-	implicit def Shorten2EnhancedShorten( shorten: Shorten ) = new EnhancedShorten( shorten )
+    implicit def Shorten2EnhancedShorten( shorten: Shorten ) = new EnhancedShorten( shorten )
 
 	/**
 	 * Row parser to extract a Shorten object from a database row result
 	 **/
-	val simple = {
-    	get[Long]( "shorten.id" ) ~ get[String]( "shorten.url" ) map { 
-    		case id ~ url => Shorten( id, url ) 
-    	}
+    val simple = {
+        get[Long]( "shorten.id" ) ~ get[String]( "shorten.url" ) map { 
+            case id ~ url => Shorten( id, url ) 
+        }
     }
 
     /**
      * Create a new Shorten in database if not existing or return the existing one
      **/
-	def getShortenFor( url:String ) = {
-		Shorten.findByUrl( url ).getOrElse {
-			Shorten( url = url ).save()
-		}
-	}
-
-	/**
-	 * Find a Shorten by its URL value
-	 **/
-	def findByUrl( url:String ) = DB.withConnection { implicit connection =>
-	    SQL( "select * from shorten s where s.url like {url} " ).on( "url" -> url ).as( Shorten.simple.singleOpt )
-	}
-
-	/** 
-	 * Useless method to try Play 2 parser API
-	 **/
-  	def findAllParser() = DB.withConnection { implicit connection =>
-	    SQL( "select * from shorten order by id asc" ).as( long("id") ~ str("url").map(_.toUpperCase()) map(flatten) *)
-	}
-
-	/*---------------- basic model requests ------------------------*/
-
-	/**
-	 * Return the list of existing Shorten
-	 **/
-	def findAll() = DB.withConnection { implicit connection =>
-	    SQL( "select * from shorten order by id asc" ).as( Shorten.simple * )
-	}
-
-	/**
-	 * Find a Shorten by its ID
-	 **/
-	def findById( id:Long ) = DB.withConnection { implicit connection =>
-	    SQL( "select * from shorten s where s.id = {id}" ).on( "id" -> id ).as( Shorten.simple.singleOpt )
-	} 
-
-	/**
-	 * Create a new Shorten on the database
-	 **/
-	def create( shorten: Shorten ) = DB.withConnection { implicit connection =>
-		val id: Long = Shorten.nextId()
-		SQL( "insert into shorten values ( {id}, {url} )" ).on( "id" -> id, "url" -> shorten.url ).executeUpdate()
-		( id, Shorten( id, shorten.url ) )
-	}
-
-	/**
-	 * Create or update shorten object on the database
-	 **/
-	def save( shorten:Shorten ) = {
-		if ( Shorten.findById( shorten.id ).isDefined ) {
-			Shorten.update( shorten.id, shorten )
-		} else {
-			Shorten.create( shorten )._2
-		}
-	}
-
-	/**
-	 * Delete a Shorten from database
-	 **/
-	def delete( id: Long ) = DB.withConnection { implicit connection =>
-		SQL( "delete from shorten where id = {id}" ).on( "id" -> id ).executeUpdate()
-	}
+    def getShortenFor( url:String ) = {
+        Shorten.findByUrl( url ).getOrElse {
+            Shorten( url = url ).save()
+        }
+    }
 
     /**
-	 * Delete all Shortens from database
-	 **/
-	def deleteAll() = DB.withConnection { implicit connection =>
-		SQL( "delete from shorten" ).executeUpdate()
-	}
+     * Find a Shorten by its URL value
+     **/
+    def findByUrl( url:String ) = DB.withConnection { implicit connection =>
+        SQL( "select * from shorten s where s.url like {url} " ).on( "url" -> url ).as( Shorten.simple.singleOpt )
+    }
 
-	/**
-	 * Update a shorten value on the database
-	 **/
-	def update( id: Long, shorten: Shorten ) = DB.withConnection { implicit connection =>
-  		SQL( "update shorten set url = {url} where id = {id}" ).on( "id"-> id, "url" -> shorten.url ).executeUpdate()
-  		Shorten( id, shorten.url )
-  	}
+    /** 
+     * Useless method to try Play 2 parser API
+     **/
+    def findAllParser() = DB.withConnection { implicit connection =>
+        SQL( "select * from shorten order by id asc" ).as( long("id") ~ str("url").map(_.toUpperCase()) map(flatten) *)
+    }
 
-  	/**
-  	 * Count existing Shorten from the databse
-  	 **/
-  	def count() = DB.withConnection { implicit connection =>
-  		val firstRow = SQL( "select count(*) as s from shorten" ).apply().head 
-		firstRow[Long]( "s" )
-  	}
+    /*---------------- basic model requests ------------------------*/
 
-  	/**
-  	 * Get the next unique ID for Shorten
-  	 **/
-  	def nextId() = DB.withConnection { implicit connection =>
-		SQL( "select next value for shorten_seq" ).as( scalar[Long].single )
-	}
+    /**
+     * Return the list of existing Shorten
+     **/
+    def findAll() = DB.withConnection { implicit connection =>
+        SQL( "select * from shorten order by id asc" ).as( Shorten.simple * )
+    }
 
-	/**
-	 * Check if a particular Shorten exists on the database
-	 **/
-	def exists( id: Long ) = Shorten.findById( id ).isDefined
+    /**
+     * Find a Shorten by its ID
+     **/
+    def findById( id:Long ) = DB.withConnection { implicit connection =>
+        SQL( "select * from shorten s where s.id = {id}" ).on( "id" -> id ).as( Shorten.simple.singleOpt )
+    } 
 
-	/**
-	 * Check if a particular Shorten exists on the database
-	 **/
-	def exists( shorten: Shorten ) = Shorten.findById( shorten.id ).isDefined
+    /**
+     * Create a new Shorten on the database
+     **/
+    def create( shorten: Shorten ) = DB.withConnection { implicit connection =>
+        val id: Long = Shorten.nextId()
+        SQL( "insert into shorten values ( {id}, {url} )" ).on( "id" -> id, "url" -> shorten.url ).executeUpdate()
+        ( id, Shorten( id, shorten.url ) )
+    }
+
+    /**
+     * Create or update shorten object on the database
+     **/
+    def save( shorten:Shorten ) = {
+        if ( Shorten.findById( shorten.id ).isDefined ) {
+            Shorten.update( shorten.id, shorten )
+        } else {
+            Shorten.create( shorten )._2
+        }
+    }
+
+    /**
+     * Delete a Shorten from database
+     **/
+    def delete( id: Long ) = DB.withConnection { implicit connection =>
+        SQL( "delete from shorten where id = {id}" ).on( "id" -> id ).executeUpdate()
+    }
+
+    /**
+     * Delete all Shortens from database
+     **/
+    def deleteAll() = DB.withConnection { implicit connection =>
+        SQL( "delete from shorten" ).executeUpdate()
+    }
+
+    /**
+     * Update a shorten value on the database
+     **/
+    def update( id: Long, shorten: Shorten ) = DB.withConnection { implicit connection =>
+        SQL( "update shorten set url = {url} where id = {id}" ).on( "id"-> id, "url" -> shorten.url ).executeUpdate()
+        Shorten( id, shorten.url )
+    }
+
+    /**
+     * Count existing Shorten from the databse
+     **/
+    def count() = DB.withConnection { implicit connection => 
+        val firstRow = SQL( "select count(*) as s from shorten" ).apply().head 
+        firstRow[Long]( "s" )
+    }
+
+    /**
+     * Get the next unique ID for Shorten
+     **/
+    def nextId() = DB.withConnection { implicit connection =>
+        SQL( "select next value for shorten_seq" ).as( scalar[Long].single )
+    }
+
+    /**
+     * Check if a particular Shorten exists on the database
+     **/
+    def exists( id: Long ) = Shorten.findById( id ).isDefined
+
+    /**
+     * Check if a particular Shorten exists on the database
+     **/
+    def exists( shorten: Shorten ) = Shorten.findById( shorten.id ).isDefined
 }
